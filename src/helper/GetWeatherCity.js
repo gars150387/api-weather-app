93a7faf8a180646569a07b5862c6f957304972ae
Gustavo <gar.santeliz@gmail.com>
2022-03-07 18:24:49 -0500

    destructuring data from array

---
+++ src/helper/GetWeatherCity.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { GetWeatherItem } from "../components/CityWeatherItem";
+
+export const GetWeatherCity = ({ city }) => {
+
+    const [fetchedWeather, setFetchedWeather] = useState([])
+
+    useEffect(() => {
+        GetWeather();
+    }, [])
+
+    const GetWeather = async () => {
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=acarigua&appid=750f844a271e6a0e4d5caf0107a40189`
+        const response = await fetch(url);
+        const data = await response.json()
+        console.log({data})
+
+        const {main} = data;
+        console.log({main});
+
+        const {weather} = data;
+        console.log(weather)
+
+        const weatherInfo = weather.map(({id, description, icon, main}) => {
+            return {
+                id: id,
+                description: description,
+                icon: icon,
+                main: main
+            }
+        })
+
+        console.log( 'weatherInfo', weatherInfo )
+        setFetchedWeather( weatherInfo )
+
+        console.log('fetchedWeather', fetchedWeather )
+    }
+
+    return (
+        <div>
+            <h4>{city}</h4>
+            <ol>
+                {fetchedWeather.map(item => {
+                    return (
+                        <GetWeatherItem 
+                        key={ item.id} 
+                        { ...item } />
+                    )
+                })}
+            </ol>
+        </div>
+    )
+}
