309f051fa0ece50075d485f511645af82c6e670f
Gustavo <gar.santeliz@gmail.com>
2022-03-14 21:00:20 -0400

    descriptions added

--- src/components/CityWeatherItem.js
+++ src/components/CityWeatherItem.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Card } from 'react-bootstrap'
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const GetWeatherItem = ({ description, main }) => {
 
     console.log({ description, main })
     return (
         <div>
-            <h3> { main } </h3>
-            <h3> { description } </h3>
+            <h3> main: { main } </h3>
+            <h3> description: { description } </h3>
         </div>
     )
 
